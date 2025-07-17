97bb2a36d7a43386f6927df3661d46fb603f4f22
Gilvaneide Medeiros <gilvaneide.m.bertaccini@gmail.com>
2025-07-17 00:11:47 -0300

    Novas correções e uso de lambdas

--- src/main/java/br/com/gilvaneide/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/gilvaneide/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,11 @@
 package br.com.gilvaneide.screenmatch;
 
 // Importações necessárias para a classe
-import br.com.gilvaneide.screenmatch.model.DadosSerie;  // Classe que representa os dados de uma série
-import br.com.gilvaneide.screenmatch.model.DadosEpisodio;  // Classe que representa os dados de um episódio
-import br.com.gilvaneide.screenmatch.model.DadosTemporada;
-import br.com.gilvaneide.screenmatch.service.ConsumoApi;  // Classe para consumir APIs externas
-import br.com.gilvaneide.screenmatch.service.ConverteDados;  // Classe para converter JSON em objetos Java
+
+import br.com.gilvaneide.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Classe principal da aplicação Screenmatch.
@@ -27,34 +21,21 @@ public class ScreenmatchApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);  // Inicializa a aplicação Spring Boot
     }
-    /**
-     * Método executado após a inicialização do Spring.
-     * Contém a lógica principal da aplicação.
-     */
+
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoApi(); // Cria uma instância para consumir APIs
+        Principal principal = new Principal();
+        principal.exibMenu();
 
-        var jsonSerie = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9de8e5d7");  //Obtém dados de uma série específica da API do OMDB
-        System.out.println("\nJSON bruto da série: " + jsonSerie);
-        ConverteDados conversor = new ConverteDados(); // Cria um conversor de JSON para objetos Java
-        DadosSerie dados = conversor.obterDados(jsonSerie, DadosSerie.class); //Converte o JSON da série para um objeto DadosSerie
-        System.out.println("\nDados da série convertidos: " + dados);
-
-        var jsonEpisodio = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=9de8e5d7"); //Obtém dados de um episódio específico da API do OMDB
-        System.out.println("\nJSON bruto do episódio: " + jsonEpisodio);
-        DadosEpisodio dadosEpisodio = conversor.obterDados(jsonEpisodio, DadosEpisodio.class);  // Converte o JSON do episódio para um objeto DadosEpisodio
-        System.out.println("\nDados do episódio convertidos: " + dadosEpisodio);
-
-        List<DadosTemporada> temporadas = new ArrayList<>(); //Cria lista com os dados obtidos de uma temporada específica da API do OMDB
-        for (int i = 1; i <= dados.totalTemporadas(); i++) {
-            var jsonTemporada = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=9de8e5d7");
-            DadosTemporada dadosTemporada = conversor.obterDados(jsonTemporada, DadosTemporada.class); // 6. Converte o JSON da temporada para um objeto DadosTemporada
-            temporadas.add(dadosTemporada); // Adiciona a temporada ao array de temporadas
-           }
-
-        System.out.println("\nDADOS DE TODAS AS TEMPORADAS: ");
-        temporadas.forEach(System.out::println);
+//        List<DadosTemporada> temporadas = new ArrayList<>(); //Cria lista com os dados obtidos de uma temporada específica da API do OMDB
+//        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+//            var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=9de8e5d7");
+//            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class); // 6. Converte o JSON da temporada para um objeto DadosTemporada
+//            temporadas.add(dadosTemporada); // Adiciona a temporada ao array de temporadas
+//           }
+//
+//        System.out.println("\nDADOS DE TODAS AS TEMPORADAS: ");
+//        temporadas.forEach(System.out::println);
     }
 }
 
---
+++ src/main/java/br/com/gilvaneide/screenmatch/principal/Principal.java
@@ -0,0 +1,50 @@
+package br.com.gilvaneide.screenmatch.principal;
+
+import br.com.gilvaneide.screenmatch.model.DadosEpisodio;
+import br.com.gilvaneide.screenmatch.model.DadosSerie;
+import br.com.gilvaneide.screenmatch.model.DadosTemporada;
+import br.com.gilvaneide.screenmatch.service.ConsumoApi;
+import br.com.gilvaneide.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String APIC_KEY = "&apikey=9de8e5d7";
+
+    public void exibMenu() {
+        System.out.println("\nDigite o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + APIC_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        //Cria lista com os dados obtidos de uma temporada específica da API do OMDB
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            var jsonTemporada = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + APIC_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(jsonTemporada, DadosTemporada.class); // 6. Converte o JSON da temporada para um objeto DadosTemporada
+            temporadas.add(dadosTemporada); // Adiciona a temporada ao array de temporadas
+        }
+//
+//        System.out.println("\nDADOS DE TODAS AS TEMPORADAS: ");
+//        temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        System.out.println("\nDADOS DE TODAS AS TEMPORADAS: ");
+        temporadas.forEach(System.out::println);
+    }
+}
